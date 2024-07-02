ca0a4ab12b0303f1cf64ece3d02cfcb7ea034ce1
RafaelHDSV <rafaebolt@gmail.com>
2024-07-02 10:26:14 -0300

    fix: fixing bugs to deploy

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -13,8 +13,8 @@ const Movie = () => {
     const [backgroundHeader] = useState(false)
 
     // selecionar id do filme pela URL
-    let idMovie = window.location.href.substring(40, 50)
-    console.log(window.location.href.substring(40, 50));
+    let idMovie = window.location.href.substring(49, 60)
+    console.log(window.location.href.substring(49, 60));
 
     // selecionar o tipo do filme pela URL
     let type = window.location.href.substring(28, 30)
